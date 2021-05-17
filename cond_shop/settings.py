8d98e08336671a701555a891191c84978a78de9f
ymoskovchuk <y.a.moskovchuk@gmail.com>
2021-05-17 23:18:13 +0300

    Added base template

--- cond_shop/settings.py
+++ cond_shop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_functions'
+    'main_functions',
+
 ]
 
 MIDDLEWARE = [
--- main_functions/urls.py
+++ main_functions/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import *
 
-urlpatterns = []
+urlpatterns = [
+    path('', test_view, name='index')
+]
